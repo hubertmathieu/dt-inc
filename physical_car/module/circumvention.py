12f717bc892afcfabd8824305fa7ab22502ea18b
William Pepin <wpepin00@gmail.com>
2023-11-27 10:01:56 -0500

    added circumvention

---
+++ physical_car/module/circumvention.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+class Circumvention:
+    def __init__(self, wheelbase_m, tire_width_m, default_object_size):
+        self._wheelbase_m = wheelbase_m
+        self._tirewidth_m = tire_width_m
+        self._default_circumvention_distance = default_object_size
+
+    def steering_for_circumvention(self, distance_from_object, sampling_time, speed, circumvention_distance=None):
+        if circumvention_distance == None:
+            circumvention_distance = self._default_circumvention_distance
+        rel_position_x = self.define_position_needed(distance_from_object, circumvention_distance, sampling_time, speed)
+
+        distance_travelled = speed*sampling_time
+        
+        delta_position_x = np.diff(rel_position_x)
+        current_angle = np.arcsin(delta_position_x/distance_travelled)
+        angle_to_travel_r = np.diff(current_angle)
+        angle_to_travel_r = np.concatenate(([0],angle_to_travel_r,[0]))
+        
+
+        circle_to_travel_ratio = angle_to_travel_r/(2*np.pi)
+        turning_circumference_m = distance_travelled/circle_to_travel_ratio
+        turning_radius_m = turning_circumference_m/(2*np.pi)
+        stuff_that_goes_inside_arcsin = self._wheelbase_m/(turning_radius_m-self._tirewidth_m/2)
+        steering_angle = np.arcsin(stuff_that_goes_inside_arcsin)
+        
+
+        end_of_gaussian = int(len(steering_angle)*0.58)
+        time_to_go_linear = np.linspace(steering_angle[end_of_gaussian],0, int(1/sampling_time)/3)
+        steering_angle = np.concatenate([steering_angle[20:end_of_gaussian], time_to_go_linear])
+        return steering_angle
+
+    def define_position_needed(self, distance_from_object, circumvention_distance, sampling_time, speed):
+        overall_distance_travelled_m = distance_from_object * 2
+        seconds_travelled = overall_distance_travelled_m / speed
+
+        a = circumvention_distance
+        b = distance_from_object
+        c = distance_from_object / 5 # random constant
+
+        # using a gaussian to define a relative x,y position function
+        positions_y_m = np.linspace(0,overall_distance_travelled_m, int(seconds_travelled/sampling_time))
+        position_x_m = a * np.exp(-(((positions_y_m-b)**2) / ((2*c)**2)))
+
+        return position_x_m
+
